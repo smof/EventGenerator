70d3b9a25041695905f055cb835291856596f841
smof <simon_moffatt@warpmail.net>
2012-05-18 12:09:34 +0100

    rearranged run through

--- EventGenerator.rb
+++ EventGenerator.rb
@@ -3,43 +3,58 @@
 #requires an ID file and Transactions file to be presented
 
 #requires
+require 'rubygems'
+require 'date'
 require 'csv'
 
 #Globals and constants#######################################################################
 IDENTITIES="ids.csv" #one user id and their corresponding ip address eg "smof,10.1.3.3"
-EVENTS_PER_USER=500 #events spread across the entire date range randomly
+EVENTS_PER_USER=10 #events spread across the entire date range randomly
 TRANSACTIONS="transactions.csv" #one transaction per line
 RESOURCE="PIX" #name of resource
-START_DATE="12/04/01" #YY/MM/DD
-END_DATE="12/04/30" #YY/MM/D
-TYPE="csv" #options include: syslog, tab, name_value 
+START_DATE="2012/04/01" #YY/MM/DD
+END_DATE="2012/04/07" #YY/MM/DD
+TYPE="name_value" #options include: syslog, tab, name_value 
 #non-configurable
 OUTPUT_FILE="#{RESOURCE}_generated_events_#{TYPE}.dat"
 CONSTRUCTED_EVENTS=[] #where the final events are pumped before output
 #Globals and constants#######################################################################
 
-
 def read_ids
 
+  puts "Reading userids file..."
+
   #iterate over csv of identies and ip's, each row is passed to row[] and each field in row passed to generate_event
-  CSV.foreach(IDENTITIES,{:col_sep => ","}) do |row|
+  CSV.foreach(IDENTITIES,{:col_sep => ",", :row_sep => "\n"}) do |row|
     
     generate_events(row[0],row[1])
         
   end
+
+  puts "Reading userids file...FINISHED"
+
+  #push events back out to new data file
+  #write_events
   
 end
 
 
 def read_transactions
   
+  puts "Reading transactions file..."
+
   @transactions =[]
   CSV.foreach(TRANSACTIONS) do |row|
     
     @transactions << row #push transaction pool into an array
     
   end
-  
+
+  puts "Reading transactions file...FINISHED"	   
+
+  #read the id data file
+# ' read_ids
+
 end
 
 
@@ -49,7 +64,7 @@ def get_date
   #array of all dates in range
   date_range = (Date.parse(START_DATE)..Date.parse(END_DATE)).to_a
   #pulls out a random date based on the Kernel randomizer going now higher than the array length
-  date_range[Kernel.rand(date_range.length-1)]
+  date_range[Kernel.rand(date_range.length-1)].to_s
         
 end
 
@@ -72,6 +87,7 @@ end
 #creates a bunch events for a particular user
 def generate_events(user,ip)
 
+  puts "Generating Events..."
 
   1.upto(EVENTS_PER_USER) {
       
@@ -79,6 +95,8 @@ def generate_events(user,ip)
     CONSTRUCTED_EVENTS << construct_event(get_date, get_time, user, ip, get_transaction) 
     
   }
+
+  puts "Generating Events...FINISHED"
     
 end
 
@@ -141,10 +159,14 @@ end
 #writes out to new events file specific for that resource
 def write_events
   
+  puts "Writing Events..."
+
   new_events_file = File.open(OUTPUT_FILE, 'w')
   CONSTRUCTED_EVENTS.each do |event| new_events_file.puts event end #basic puts but driven to open file
   new_events_file.close #closes
-  
+
+  puts "Writing Events...FINISHED"
+
 end
 
 
@@ -156,10 +178,8 @@ end
 
 puts "Starting event generator #{Time.now}"
 puts "###############################################################################"
-puts "Reading Data files..."
 read_transactions
 read_ids
 write_events
-puts "Creating Events..."
 puts "###############################################################################"
 puts "Ended event generator #{Time.now}"
